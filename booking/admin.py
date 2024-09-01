477dff61c2988b0c9422b4f877ec5bc11a9f38c3
Daniel Pribula <daniel.pribula@gmail.com>
2024-09-01 21:13:51 +0000

    urls edit, summernote install

--- booking/admin.py
+++ booking/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Reservation
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reservation_date', 'guest_count', 'status', 'created_on', 'updated_on')
+    search_fields = ['user__username', 'reservation_date', 'guest_count', 'status']
+    list_filter = ('status', 'reservation_date', 'created_on')
+    date_hierarchy = 'reservation_date'
+    readonly_fields = ('created_on', 'updated_on', 'created_by', 'edited_by')
+    
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # If the object is new (being created)
+            obj.created_by = request.user
+        obj.edited_by = request.user
+        super().save_model(request, obj, form, change)
 
 # Register your models here.
-admin.site.register(Reservation)
+# admin.site.register(Reservation)
--- codestar/urls.py
+++ codestar/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     #path('booking/', my_booking, name='booking'),
     path("", include("booking.urls"), name='booking-urls'),
 ]
